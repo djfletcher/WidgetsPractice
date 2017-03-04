75625ed9cb4dd6493f002be0929316bbc2cb687f
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-03 16:01:09 -0800

    Begin weather widget

--- widgets.jsx
+++ widgets.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Congrats from './congrats';
 import Tabs from './tabs';
+import Weather from './weather';
+
+
 
 document.addEventListener("DOMContentLoaded", e => {
   let title = ["cheesecake", "tiramisu", "real chia pudding"];
@@ -16,6 +19,10 @@ document.addEventListener("DOMContentLoaded", e => {
     allHash.push(hash);
   }
 
+  let tabs = <Tabs data={allHash}/>;
+  let weather = <Weather/>;
   const root = document.getElementById("root");
-  ReactDOM.render(<Tabs data={allHash}/>, root);
+  ReactDOM.render(Widgets(tabs, weather), root);
 });
+
+const Widgets = (tabs, weather) => <div>{tabs}{weather}</div>;
